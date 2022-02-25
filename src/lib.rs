b184bafcff34591ed4f7b876e82837d105f7e296
Linfeng Qian <thewawar@gmail.com>
2022-02-25 11:48:07 +0800

    Copy code

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,6 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+mod keystore;
+pub use keystore::{
+    zeroize_privkey, zeroize_slice, CipherParams, Crypto, DerivedKeySet, Error as KeyStoreError,
+    KdfParams, Key, KeyChain, KeyStore, KeyTimeout, MasterPrivKey, ScryptParams, ScryptType,
+    CKB_ROOT_PATH,
+};
